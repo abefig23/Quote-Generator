0443d6e21822524bc394fdb9aa40df0c72ed7f15
abefig23 <abraham.figueroa018@gmail.com>
2023-08-22 22:36:04 -0700

    added loader

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader')
 
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 function newQuote() {
+  loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     if (!quote.author) {
@@ -22,16 +34,17 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    //add sweetalert when erroring
   }
 }
 
